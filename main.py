f34810d4f0aa6103a33a2101f96f112281540876
Morgan Pigeron <m.pigeron@hotmail.fr>
2020-03-03 12:15:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,40 +10,28 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 bot = commands.Bot(command_prefix = ">")
 
-boRobot = Bot(language="EN")
+boRobot = Bot()
 
 @bot.event
 async def on_ready():
-    if boRobot.language == "FR":
-        print(constants.TEXT["FR"]["on_ready"])
-    if boRobot.language == "EN":
-        print(constants.TEXT["EN"]["on_ready"])
+    print(constants.TEXT["FR"]["on_ready"])
 
+
+"""
 @bot.event
 async def on_member_join(member):
-    if boRobot.language == "FR":
-        print(str(member) +" "+ constants.TEXT["FR"]["on_member_join"])
-    if boRobot.language == "EN":
-        print(str(member) +" "+ constants.TEXT["EN"]["on_member_join"])
+    print(str(member) +" "+ constants.TEXT["FR"]["on_member_join"])
+
 
 @bot.event
 async def on_member_remove(member):
-    if boRobot.language == "FR":
-        print(str(member) + constants.TEXT["FR"]["on_member_remove"])
-    if boRobot.language == "EN":
-        print(str(member) + constants.TEXT["EN"]["on_member_remove"])
-
-@bot.command()
-async def ping(ctx):
-    if boRobot.language == "FR":
-        await ctx.send( constants.TEXT["FR"]["ping"] + round(bot.latency*1000) + "ms")
-    if boRobot.language == "EN":
-        await ctx.send( constants.TEXT["EN"]["ping"] + round(bot.latency*1000) + "ms")
-
+    print(str(member) + constants.TEXT["FR"]["on_member_remove"])
 """
-@bot.command(aliases=['8ball', 'ditMoi'])
-async def _8ball(ctx, *args, **kwargs):
-"""  
+
+@bot.command(aliases=['ping', 'latency', 'latence'])
+async def _ping(ctx):
+    await ctx.send( constants.TEXT["FR"]["ping"] + str(round(bot.latency*1000)) + "ms")
+
 
 bot.run(key.getKey(os.path.join(dir_path, "key.txt")))
 
