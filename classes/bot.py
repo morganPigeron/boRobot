c97299f5d97d20d62a2fc1355e7c08d2cd4a9dc7
Morgan Pigeron <m.pigeron@hotmail.fr>
2020-03-06 14:59:27 +0100

    kaamelott reading

--- classes/bot.py
+++ classes/bot.py
@@ -1,6 +1,9 @@
 
 import subprocess
 import sys
+import urllib.request
+from bs4 import BeautifulSoup
+import random
 
 class Bot():
     def __init__(self, language = "FR", talk = False):
@@ -36,3 +39,29 @@ class Bot():
 
     async def send(self, ctx, message):
         await ctx.send(message, tts=self.talk )
+
+    def getCitation(self):
+
+        #url
+        url = "http://kaamelott.over-blog.fr/pages/Repliques_cultes-2024869.html"
+
+        html = urllib.request.urlopen(url)
+
+        #parsing
+        soup = BeautifulSoup(html,"html.parser")
+
+        citations = soup.find_all("span", class_="citation")
+        
+        citationsTab = []
+        for citation in citations:
+            citationsTab.append(citation.getText())
+
+        #pick a random one
+        index = random.randint(0,len(citationsTab))
+
+        return citationsTab[index]
+
+
+if __name__ == "__main__":
+    A = Bot()
+    A.lit()
